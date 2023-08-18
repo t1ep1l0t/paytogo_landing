a0363b4ee3bbfb113ffc4380bc151636a08f2424
t1ep1l0t <alien95111@gmail.com>
2023-08-18 17:12:37 +0300

    add dist 3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -145,7 +145,7 @@ export default {
   </section>
   <section class="section faq" ref="faq">
     <div class="container">
-      <h1>faq</h1>
+      <h1>FAQ</h1>
     </div>
   </section>
 </template>
